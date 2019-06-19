a5fc1be89b78ae857c9b4dfb6089cb8cef360bb5
Daniel Boline <dboline@mediamath.com>
2019-06-19 13:26:16 -0400

    bugfix

--- scripts/episodes.sql
+++ scripts/episodes.sql
@@ -1,9 +1,11 @@
 CREATE TABLE episodes (
-    castid SERIAL PRIMARY KEY,
+    castid INTEGER NOT NULL,
     episodeid INTEGER NOT NULL,
     title TEXT NOT NULL,
     epurl TEXT NOT NULL,
     enctype TEXT NOT NULL,
     status TEXT NOT NULL,
-    epguid TEXT
+    epguid TEXT,
+    CONSTRAINT episodes_castid_episodeid_key UNIQUE (castid, episodeid),
+    CONSTRAINT episodes_castid_epurl_key UNIQUE (castid, epurl)
 )
