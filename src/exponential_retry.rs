9e56574cedd56ac2503684866730f4851557750b
Daniel Boline <ddboline@gmail.com>
2019-10-20 12:51:52 -0400

    replace custom retry implemenation with retry crate

--- src/exponential_retry.rs
+++ src/exponential_retry.rs
@@ -1,28 +1,24 @@
-use failure::{err_msg, Error};
-use rand::distributions::{Distribution, Uniform};
-use rand::thread_rng;
+use failure::{format_err, Error};
+use log::error;
 use reqwest::{Client, Response, Url};
-use std::thread::sleep;
-use std::time::Duration;
+use retry::{delay::jitter, delay::Exponential, retry};
 
 pub trait ExponentialRetry {
     fn get_client(&self) -> &Client;
 
     fn get(&self, url: &Url) -> Result<Response, Error> {
-        let mut timeout: f64 = 1.0;
-        let mut rng = thread_rng();
-        let range = Uniform::from(0..1000);
-        loop {
-            match self.get_client().get(url.clone()).send() {
-                Ok(x) => return Ok(x),
-                Err(e) => {
-                    sleep(Duration::from_millis((timeout * 1000.0) as u64));
-                    timeout *= 4.0 * f64::from(range.sample(&mut rng)) / 1000.0;
-                    if timeout >= 64.0 {
-                        return Err(err_msg(e));
-                    }
-                }
-            }
-        }
+        retry(
+            Exponential::from_millis(2)
+                .map(jitter)
+                .map(|x| x * 500)
+                .take(6),
+            || {
+                self.get_client().get(url.clone()).send().map_err(|e| {
+                    error!("Got error {:?} , retrying", e);
+                    e
+                })
+            },
+        )
+        .map_err(|e| format_err!("{:?}", e))
     }
 }
