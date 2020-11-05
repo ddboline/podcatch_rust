3f7cbf958d1a214356a45daec501b70a2d883882
Daniel Boline <ddboline@gmail.com>
2020-11-05 16:00:58 -0500

    upgrade to tokio 0.3 and tokio-postgres 0.6

--- src/exponential_retry.rs
+++ src/exponential_retry.rs
@@ -5,7 +5,9 @@ use rand::{
     thread_rng,
 };
 use reqwest::{Client, Response, Url};
-use tokio::time::{delay_for, Duration};
+use std::time::Duration;
+use tokio::time::sleep;
+use tokio_compat_02::FutureExt;
 
 #[async_trait]
 pub trait ExponentialRetry {
@@ -15,10 +17,10 @@ pub trait ExponentialRetry {
         let mut timeout: f64 = 1.0;
         let range = Uniform::from(0..1000);
         loop {
-            match self.get_client().get(url.clone()).send().await {
+            match self.get_client().get(url.clone()).send().compat().await {
                 Ok(resp) => return Ok(resp),
                 Err(err) => {
-                    delay_for(Duration::from_millis((timeout * 1000.0) as u64)).await;
+                    sleep(Duration::from_millis((timeout * 1000.0) as u64)).await;
                     timeout *= 4.0 * f64::from(range.sample(&mut thread_rng())) / 1000.0;
                     if timeout >= 64.0 {
                         return Err(err.into());
