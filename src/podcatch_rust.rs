abd196026c1970f8f65710930649f15e1b75020f
Daniel Boline <ddboline@gmail.com>
2022-09-20 23:02:09 -0400

    tokio spawn

--- src/podcatch_rust.rs
+++ src/podcatch_rust.rs
@@ -5,5 +5,7 @@ use podcatch_rust::podcatch_opts::PodcatchOpts;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    PodcatchOpts::process_args().await
+    tokio::spawn(async move {
+        PodcatchOpts::process_args().await
+    }).await.unwrap()
 }
